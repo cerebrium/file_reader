8618448e18c8046da4fc507514709386b9798e56
Nicholas shankland <nicholasshankland@Nicholass-MBP.localdomain>
2023-07-10 10:25:02 -0700

    removing un-needed stuff

--- src/index.ts
+++ src/index.ts
@@ -6,19 +6,6 @@ function main() {
   console.log("Running main")
   const [_, __, file_name] = process.argv;
   read_file_contents_by_line({ file_name, line_func: sig_bug_line_split })
-  const file_contents = (file_name)
-  console.log("file contents: ", file_contents)
-
-}
-
-function read_file_sync(file_name: string) {
-  try {
-    const file_contents = fs.readFileSync(`./Files/${file_name}`, 'utf8');
-    return file_contents;
-  } catch (e) {
-    console.error(e);
-    process.exit(1);
-  }
 }
 
 function read_file_contents_by_line({ file_name, line_func }: { file_name: string, line_func: (line: string) => void }) {
